634ad430c41ab12cc6ed502ca03886570d94a06c
Juan Manuel Spinelli <juan.spinelli@fligoo.com>
2020-12-28 13:19:56 -0300

    Test

--- boto/create_bucket.py
+++ boto/create_bucket.py
@@ -7,6 +7,10 @@ session = boto3.session.Session()
 s3_client = session.client(
     service_name='s3',
     endpoint_url='http://192.168.49.2:31566',
+    aws_access_key_id = 'AKIAIOSFODNN7EXAMPLE',
+    aws_secret_access_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+    default_region_name = 'us-west-2',
+    default_output_format = 'json',
 )
 
 region = session.region_name
